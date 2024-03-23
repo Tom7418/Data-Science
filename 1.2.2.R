f684395ccb4166d657097a53be253a2c40782dd2
Park Hyeonggyu <162649328+Tom7418@users.noreply.github.com>
2024-03-23 20:55:27 +0900

    Update 1.2.2.R

--- 1.2.2.R
+++ 1.2.2.R
@@ -1,1 +1,5 @@
-install.packages
+install.packages("readxl") # 외부 엑셀,텍스트,CSV파일등일 읽어들일수 있는 패키지 설치
+library(readxl) # readxl 패키지를 불러오는 함수 library
+
+ xel <- read_excel("D:/KR_pl.xlsx") # xel이라는 함수에 D드라이브 안에 KR_all이라는 엑셀을 읽어드려서 저장
+ View(xel) # 데이터를 볼수 있게함
